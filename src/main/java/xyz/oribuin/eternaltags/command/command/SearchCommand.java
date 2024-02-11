f301b812240fd4596509a9e51a83c286e7a5755b
Robin <oribuinn@gmail.com>
2024-02-11 23:09:46 +0000

    Fix Dependency Errors on spigot

--- src/main/java/xyz/oribuin/eternaltags/command/command/SearchCommand.java
+++ src/main/java/xyz/oribuin/eternaltags/command/command/SearchCommand.java
@@ -12,13 +12,12 @@ import org.bukkit.entity.Player;
 import xyz.oribuin.eternaltags.gui.MenuProvider;
 import xyz.oribuin.eternaltags.gui.menu.TagsGUI;
 import xyz.oribuin.eternaltags.manager.ConfigurationManager.Setting;
+import xyz.oribuin.eternaltags.manager.TagsManager;
 import xyz.oribuin.eternaltags.obj.Tag;
 
 import java.util.function.Predicate;
 
 public class SearchCommand extends RoseCommand {
-
-    private static final PlainTextComponentSerializer PLAIN = PlainTextComponentSerializer.plainText();
 
     public SearchCommand(RosePlugin rosePlugin, RoseCommandWrapper parent) {
         super(rosePlugin, parent);
@@ -38,24 +37,7 @@ public class SearchCommand extends RoseCommand {
     private Predicate<Tag> containsKeyword(String keyword) {
         return tag -> tag.getId().contains(keyword)
                       || tag.getName().contains(keyword)
-                      || tag.getDescription().contains(keyword)
-                      || this.strip(tag.getTag()).contains(keyword);
-    }
-
-    /**
-     * Strip the color codes from a string
-     *
-     * @param text The string to strip
-     * @return The stripped string
-     */
-    @SuppressWarnings("deprecation")
-    private String strip(String text) {
-        if (text == null) return null;
-
-        if (Setting.TAG_FORMATTING.getString().equalsIgnoreCase("mini_message"))
-            return PLAIN.deserialize(text).content();
-
-        return ChatColor.stripColor(text);
+                      || tag.getDescription().contains(keyword);
     }
 
     @Override
