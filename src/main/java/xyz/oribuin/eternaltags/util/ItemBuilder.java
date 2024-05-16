2967e374c4c581147b9e49c8f449d4b169522bfd
Robin <oribuinn@gmail.com>
2024-05-16 01:10:30 +0100

    optimize itembuilder, fix potion colour

--- src/main/java/xyz/oribuin/eternaltags/util/ItemBuilder.java
+++ src/main/java/xyz/oribuin/eternaltags/util/ItemBuilder.java
@@ -24,13 +24,16 @@ import java.util.Map;
 public class ItemBuilder {
 
     private final ItemStack item;
+    private ItemMeta meta;
 
     public ItemBuilder(Material material) {
         this.item = new ItemStack(material);
+        this.meta = this.item.getItemMeta();
     }
 
     public ItemBuilder(ItemStack item) {
         this.item = item.clone();
+        this.meta = this.item.getItemMeta();
     }
 
     public ItemBuilder setMaterial(Material material) {
@@ -45,13 +48,7 @@ public class ItemBuilder {
      * @return Item.Builder.
      */
     public ItemBuilder name(@Nullable String text) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null || text == null)
-            return this;
-
-        meta.setDisplayName(text);
-        this.item.setItemMeta(meta);
-
+        this.meta.setDisplayName(text);
         return this;
     }
 
@@ -62,12 +59,7 @@ public class ItemBuilder {
      * @return Item.Builder.
      */
     public ItemBuilder lore(@Nullable List<String> lore) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null || lore == null)
-            return this;
-
-        meta.setLore(lore);
-        this.item.setItemMeta(meta);
+        this.meta.setLore(lore);
         return this;
     }
 
@@ -100,12 +92,7 @@ public class ItemBuilder {
      * @return Item.Builder
      */
     public ItemBuilder enchant(Enchantment ench, int level) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null) return this;
-
-        meta.addEnchant(ench, level, true);
-        this.item.setItemMeta(meta);
-
+        this.meta.addEnchant(ench, level, true);
         return this;
     }
 
@@ -117,14 +104,7 @@ public class ItemBuilder {
      */
 
     public ItemBuilder enchant(Map<Enchantment, Integer> enchantments) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null) return this;
-
-        for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
-            meta.addEnchant(entry.getKey(), entry.getValue(), true);
-        }
-
-        this.item.setItemMeta(meta);
+        enchantments.forEach(this::enchant);
         return this;
     }
 
@@ -146,13 +126,7 @@ public class ItemBuilder {
      * @return Item.Builder
      */
     public ItemBuilder flags(ItemFlag[] flags) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null) return this;
-
-        meta.removeItemFlags(ItemFlag.values());
-        meta.addItemFlags(flags);
-        this.item.setItemMeta(meta);
-
+        this.meta.removeItemFlags(ItemFlag.values());
         return this;
     }
 
@@ -164,11 +138,7 @@ public class ItemBuilder {
      * @return Item.Builder
      */
     public ItemBuilder unbreakable(boolean unbreakable) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null) return this;
-
-        meta.setUnbreakable(unbreakable);
-        item.setItemMeta(meta);
+        this.meta.setUnbreakable(unbreakable);
         return this;
     }
 
@@ -180,13 +150,8 @@ public class ItemBuilder {
     public ItemBuilder glow(boolean b) {
         if (!b) return this;
 
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null) return this;
-
-        meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1, true);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        this.item.setItemMeta(meta);
-
+        this.meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1, true);
+        this.meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         return this;
     }
 
@@ -200,12 +165,8 @@ public class ItemBuilder {
         if (item.getType() != Material.PLAYER_HEAD || texture == null)
             return this;
 
-        final SkullMeta skullMeta = (SkullMeta) item.getItemMeta();
-        if (skullMeta == null)
-            return this;
-
+        if (!(this.meta instanceof SkullMeta skullMeta)) return this;
         SkullUtils.setSkullTexture(skullMeta, texture);
-        this.item.setItemMeta(skullMeta);
         return this;
     }
 
@@ -216,46 +177,33 @@ public class ItemBuilder {
      * @return Item.Builder
      */
     public ItemBuilder owner(OfflinePlayer owner) {
-        if (item.getType() != Material.PLAYER_HEAD)
-            return this;
-
-        final SkullMeta skullMeta = (SkullMeta) item.getItemMeta();
-        if (skullMeta == null || owner == null || skullMeta.getOwningPlayer() != null)
-            return this;
+        if (item.getType() != Material.PLAYER_HEAD) return this;
+        if (owner == null) return this;
+        if (!(this.meta instanceof SkullMeta skullMeta)) return this;
 
         skullMeta.setOwningPlayer(owner);
-        this.item.setItemMeta(skullMeta);
         return this;
     }
 
     public ItemBuilder model(int model) {
-        final ItemMeta meta = this.item.getItemMeta();
-        if (meta == null || model <= 0)
-            return this;
-
-        meta.setCustomModelData(model);
-        this.item.setItemMeta(meta);
+        this.meta.setCustomModelData(model);
         return this;
     }
 
     public ItemBuilder potion(PotionEffectType effectType, int duration, int amp) {
-        if (!(this.item.getItemMeta() instanceof PotionMeta meta))
-            return this;
+        if (!(this.meta instanceof PotionMeta potionMeta)) return this;
 
-        meta.addCustomEffect(new PotionEffect(effectType, duration, amp), true);
-        this.item.setItemMeta(meta);
+        potionMeta.addCustomEffect(new PotionEffect(effectType, duration, amp), true);
         return this;
     }
 
     public ItemBuilder color(Color color) {
-        if (this.item.getItemMeta() instanceof PotionMeta meta) {
-            meta.setColor(color);
-            this.item.setItemMeta(meta);
+        if (this.meta instanceof PotionMeta potionMeta) {
+            potionMeta.setColor(color);
         }
 
-        if (this.item.getItemMeta() instanceof LeatherArmorMeta meta) {
-            meta.setColor(color);
-            this.item.setItemMeta(meta);
+        if (this.item.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta) {
+            leatherArmorMeta.setColor(color);
         }
 
         return this;
@@ -267,6 +215,7 @@ public class ItemBuilder {
      * @return The ItemStack
      */
     public ItemStack build() {
+        this.item.setItemMeta(this.meta);
         return this.item;
     }
 
