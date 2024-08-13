cbc208ff743e81ad83abba8fc17521e7a173cbfc
Robin <oribuinn@gmail.com>
2024-08-13 19:17:03 +0100

    Add ChatInjector to EternalTags

--- src/main/java/xyz/oribuin/eternaltags/manager/CommandManager.java
+++ src/main/java/xyz/oribuin/eternaltags/manager/CommandManager.java
@@ -3,18 +3,6 @@ package xyz.oribuin.eternaltags.manager;
 import dev.rosewood.rosegarden.RosePlugin;
 import dev.rosewood.rosegarden.command.framework.BaseRoseCommand;
 import dev.rosewood.rosegarden.manager.AbstractCommandManager;
-import xyz.oribuin.eternaltags.command.impl.CategoriesCommand;
-import xyz.oribuin.eternaltags.command.impl.ClearCommand;
-import xyz.oribuin.eternaltags.command.impl.ConvertCommand;
-import xyz.oribuin.eternaltags.command.impl.CreateCommand;
-import xyz.oribuin.eternaltags.command.impl.DeleteCommand;
-import xyz.oribuin.eternaltags.command.impl.EditCommand;
-import xyz.oribuin.eternaltags.command.impl.FavoriteCommand;
-import xyz.oribuin.eternaltags.command.impl.RandomCommand;
-import xyz.oribuin.eternaltags.command.impl.ReloadCommand;
-import xyz.oribuin.eternaltags.command.impl.SearchCommand;
-import xyz.oribuin.eternaltags.command.impl.SetAllCommand;
-import xyz.oribuin.eternaltags.command.impl.SetCommand;
 import xyz.oribuin.eternaltags.command.impl.TagsCommand;
 
 import java.util.List;
